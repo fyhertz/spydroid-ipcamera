6e05c6f1fa7066299c270b963796b214e8524a1a
fyhertz <fyhertz@gmail.com>
2013-04-18 05:31:08 -0400

    ACRA disabled.

--- src/net/majorkernelpanic/spydroid/SpydroidApplication.java
+++ src/net/majorkernelpanic/spydroid/SpydroidApplication.java
@@ -79,7 +79,7 @@ public class SpydroidApplication extends android.app.Application {
 
 		// The following line triggers the initialization of ACRA
 		// Please do not uncomment this line unless you change the form id or I will receive your crash reports !
-		ACRA.init(this);
+		//ACRA.init(this);
 
 		sApplication = this;
 
